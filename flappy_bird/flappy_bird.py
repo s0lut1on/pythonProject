24316d9c44c72283fc6482384adefcfabadcdecd
nam.vp <nam.vp@anlab.pro>
2020-03-16 16:46:29 +0700

    Update flappy bird with class

--- flappy_bird/flappy_bird.py
+++ flappy_bird/flappy_bird.py
@@ -3,12 +3,6 @@ import time
 import threading
 import random
 
-white=(255,255,255)
-black=(0,0,0)
-red=(255,0,0)
-green=(0,255,0)
-blue=(0,0,255)
-
 #init pygame
 pygame.init()
 
@@ -22,35 +16,79 @@ pygame.display.set_icon(icon)
 
 #init game value
 running = True
-#int background
-background = pygame.image.load('bg.png')
-background = pygame.transform.scale(background, (800, 600))
-backgroundX = 0
-backgroundX2 = background.get_width() - 50
 
-#init bird
-countBird = 1
-birdX = 200
-birdY = 300
+class Bird:
+    # x = 200
+    # y = 300
+    # count = 1
+    def __init__(self):
+        self.x = 200
+        self.y = 300
+        self.count = 1
+        self.birdImage = pygame.image.load(str(self.count)+'.png')
+        self.birdImage = pygame.transform.scale(self.birdImage, (50, 50))
+        screen.blit(self.birdImage, (self.x, self.y))
+        
 
-#init pipe
-spacing_pipe = 80
-pipe_x = 0
-pipe_width = 30
-pipe_up = pygame.image.load('tp.png')
-pipe_down = pygame.image.load('bp.png')
-pipe_up_height = random.randrange(50, 310, 10)
-pipe_down_height = background.get_height() - pipe_up_height - spacing_pipe
+    def update(self):
+        self.y += 8
+        self.count += 1
+        if self.count == 5:
+            self.count = 1
+        self.birdImage = pygame.image.load(str(self.count)+'.png')
+        self.birdImage = pygame.transform.scale(self.birdImage, (50, 50))
+        screen.blit(self.birdImage, (self.x, self.y))
 
-def update_background(backgroundX, backgroundX2):
-    backgroundX -= 1.4
-    backgroundX2 -= 1.4
-    if backgroundX < background.get_width() * -1:  # If our bg is at the -width then reset its position
-        backgroundX = background.get_width()
-    if backgroundX2 < background.get_width() * -1:
-        backgroundX2 = background.get_width()
+class BackGround:
+    def __init__(self):
+        self.background = pygame.image.load('bg.png')
+        self.background = pygame.transform.scale(self.background, (800, 600))
+        self.x = 0
+        self.x2 = self.background.get_width() - 50
+        self.width = self.background.get_width()
+        self.height = self.background.get_height()
+        screen.blit(self.background, (self.x, 0))
+        screen.blit(self.background, (self.x2, 0))
+
+    def update(self):
+        self.x -= 1.4
+        self.x2 -= 1.4
+        if self.x < self.background.get_width() * -1:  # If our bg is at the -width then reset its position
+            self.x = self.background.get_width()
+        if self.x2 < self.background.get_width() * -1:
+            self.x2 = self.background.get_width()
+            
+        screen.blit(self.background, (self.x, 0))
+        screen.blit(self.background, (self.x2, 0))
+
+class Pipe:
+    def __init__(self, backgroundWidth, backgroundHeight):
+        self.backgroundHeight = backgroundHeight
+        self.backgroundWidth = backgroundWidth
+        self.spacing = 80
+        self.x = 0
+        self.width = 30
+        self.imgUp = pygame.image.load('tp.png')
+        self.imgDown = pygame.image.load('bp.png')
+        self.upHeight = random.randrange(50, 310, 10)
+        self.downHeight = self.backgroundHeight - self.upHeight - self.spacing
+
+    def update(self):
+        if (self.x == 0):
+            self.x = self.backgroundWidth - 50
+            self.upHeight = random.randrange(50, 400, 10)
+            self.downHeight = self.backgroundHeight - self.upHeight - self.spacing
+            self.imgUp = pygame.transform.scale(self.imgUp, (self.width, self.upHeight))
+            self.imgDown = pygame.transform.scale(self.imgDown, (self.width,  self.downHeight))
+        else:
+            screen.blit(self.imgUp, (self.x, 0))
+            screen.blit(self.imgDown, (self.x, self.backgroundHeight - self.downHeight))
+            self.x -= 10
 
 #game loop
+background = BackGround()
+bird = Bird()
+pipe = Pipe(background.width, background.height)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -58,37 +96,15 @@ while running:
 
     keys=pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        birdY -= 28
+        bird.y -= 28
 
-    #background create
-    update_background(backgroundX, backgroundX2)
-    screen.blit(background, (backgroundX, 0))
-    screen.blit(background, (backgroundX2, 0))
-    
-    #bird create
-    birdImage = pygame.image.load(str(countBird)+'.png')
-    birdImage = pygame.transform.scale(birdImage, (50, 50))
-    screen.blit(birdImage, (birdX, birdY))
-    birdY += 8
-    countBird += 1
-    if (countBird == 5):
-        countBird = 1
-    
-    #pipe create
-    if (pipe_x == 0):
-        pipe_x = background.get_width() - 50
-        pipe_up_height = random.randrange(50, 400, 10)
-        pipe_down_height = 600 - pipe_up_height - spacing_pipe
-        pipe_up = pygame.transform.scale(pipe_up, (pipe_width, pipe_up_height))
-        pipe_down = pygame.transform.scale(pipe_down, (pipe_width,  pipe_down_height))
-    else:
-        screen.blit(pipe_up, (pipe_x, 0))
-        screen.blit(pipe_down, (pipe_x, 600 - pipe_down_height))
-        pipe_x -= 10
+    background.update()
+    bird.update()
+    pipe.update()
 
     #check bird touch pipe
-    if birdX >= pipe_x and birdX <= pipe_x + pipe_width:
-        if (birdY <= pipe_up_height or birdY >= pipe_up_height + spacing_pipe):
+    if bird.x >= pipe.x and bird.x <= pipe.x + pipe.width:
+        if (bird.y <= pipe.upHeight or bird.y >= pipe.upHeight + pipe.spacing):
             running = False
 
     pygame.display.update()
