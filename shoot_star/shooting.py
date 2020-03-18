f1d61cb3551a1da569135f533ac2781c7b606659
nam.vp <nam.vp@anlab.pro>
2020-03-18 16:31:45 +0700

    Update logic shoot into enemy, revert for loop

--- shoot_star/shooting.py
+++ shoot_star/shooting.py
@@ -87,11 +87,10 @@ while running:
         ship.x -= 10
 
     #check shoot star
-    for i in range(0, len(listBullet)):
-        for j in range(0, len(listStaticEnemy)):
-            if (listStaticEnemy[j].x > listBullet[i].x and listStaticEnemy[j].x < listBullet[i].x 
+    for i in range(len(listBullet)-1, -1, -1):
+        for j in range(len(listStaticEnemy)-1, -1, -1):
+            if (listStaticEnemy[j].x > listBullet[i].x and listStaticEnemy[j].x < (listBullet[i].x + 30)
                 and (listStaticEnemy[j].y + 30) > listBullet[i].y and listStaticEnemy[j].y < listBullet[i].y):
-                listBullet.pop(i)
                 listStaticEnemy.pop(j)
     
     #draw item
